47f2840f40dd7b6503d7ac4773472c3155c924f5
Duncan McGreggor <duncan.mcgreggor@rackspace.com>
2014-02-04 13:48:49 -0600

    Set up initial project.

---
+++ skellib/meta.py
@@ -0,0 +1,10 @@
+display_name = "Skeleton Library"
+library_name = "skellib"
+version = "0.0.1"
+author = "Carla Coder"
+author_email = "carla@codes.com"
+license = "Open All The Things!"
+url = "https://github.com/carlacoder/skellib"
+description = "A Simple Skeleton Project for Python"
+long_description = description
+requires = []
